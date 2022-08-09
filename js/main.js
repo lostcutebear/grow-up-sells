1880cd4e5db4cfd58afd4fb795f74487f603f7c5
Nikolay <pythonscript999@gmail.com>
2022-08-10 01:22:46 +0300

    main file with jquery and slick-slider were added for implementing slick(a pile of word `slick`, I know)

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $('.main__partners-wrapper').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1
+// });
+
+/* 
+    It doesn't work. Today, at noon, I'm gonna check out why it has bugs
+    Slick-slider is a carousel that allows users to slide through slides(tautology, I know :D)
+*/
